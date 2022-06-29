91c3b21ae01c9e051e8fb9ae6e02b970cb6018f2
ananyaAT <69104322+ananyaAT@users.noreply.github.com>
2022-06-29 17:33:16 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import postRoutes from './routes/posts.js'
+import express from 'express';
+
+const app = express();
+app.use('/posts', postRoutes);
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://ar14:iamananya14@cluster0.kcwilvl.mongodb.net/?retryWrites=true&w=majority';
+const PORT = process.env.PORT|| 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(error.message));
